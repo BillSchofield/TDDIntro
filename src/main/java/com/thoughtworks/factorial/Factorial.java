7a97c5238a529fe01f585c6e3e8f527705ba39d3
Sebastian Radloff <sradloff23@gmail.com>
2015-02-23 23:59:07 -0600

    made all tests pass

--- src/main/java/com/thoughtworks/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/factorial/Factorial.java
@@ -2,6 +2,13 @@ package com.thoughtworks.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        return 1;
+        if (i < 0){
+            throw new IllegalArgumentException("The number must be an integer 0 or greater");
+        }
+        if (i == 0){
+            return 1;
+        } else {
+            return i * compute(i-1);
+        }
     }
 }
