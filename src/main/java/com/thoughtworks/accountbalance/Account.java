340260dff5eccc5ab6b9528a75369c398f81d194
Sebastian Radloff <sradloff23@gmail.com>
2015-02-25 07:17:23 -0600

    completed first test

--- src/main/java/com/thoughtworks/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/accountbalance/Account.java
@@ -1,8 +1,23 @@
 package com.thoughtworks.accountbalance;
 
+
 /**
  * Created by sebass on 2/25/15.
  */
 public class Account {
+    private double balance;
+
+    public Account(double startingBalance){
+       balance = startingBalance;
+    }
+
+    public void depositMoney(double money){
+        this.balance += money;
+    }
+
+    public double getBalance(){
+        return this.balance;
+    }
+
 
 }
--- src/test/java/com/thoughtworks/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/accountbalance/AccountTests.java
@@ -2,12 +2,15 @@ package com.thoughtworks.accountbalance;
 
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class AccountTests {
     @Test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
         Account account = new Account(100);
         account.depositMoney(50);
-        assertThat(account.balance(),150);
+        assertThat(account.getBalance(), is(150.00));
     }
 
 //    @Test
