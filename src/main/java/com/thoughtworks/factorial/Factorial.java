d39beee859510d688994e8c400545b2a40f938a6
n-zeplo <nzeplowitz@gmail.com>
2015-04-10 13:20:56 -0400

    Refactor Code After All Tests Pass

--- src/main/java/com/thoughtworks/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/factorial/Factorial.java
@@ -3,12 +3,9 @@ package com.thoughtworks.factorial;
 public class Factorial {
     public Integer compute(int i) {
         if (i < 0) throw new IllegalArgumentException();
-        
-        if(i == 0){
+
+        if(i < 2) {
             return 1;
-        }
-        else if(i <= 2){
-            return i;
         }
         else {
             return i * compute(i - 1);
