b71d4fc0c06839ecbe79441dc5d6a9cd02b6a48e
n-zeplo <nzeplowitz@gmail.com>
2015-04-10 13:12:29 -0400

    Pass First Test Compute 1 Returns 1

--- src/main/java/com/thoughtworks/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/factorial/Factorial.java
@@ -2,6 +2,9 @@ package com.thoughtworks.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
+        if(i == 1){
+            return 1;
+        }
         return -1;
     }
 }
