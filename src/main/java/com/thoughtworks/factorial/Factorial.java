0e4ebdeb318bf57994503e1681f47ec35c76cd82
n-zeplo <nzeplowitz@gmail.com>
2015-04-10 13:20:09 -0400

    Pass Fifth Test Negative Number Raises Exception

--- src/main/java/com/thoughtworks/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/factorial/Factorial.java
@@ -2,6 +2,8 @@ package com.thoughtworks.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
+        if (i < 0) throw new IllegalArgumentException();
+        
         if(i == 0){
             return 1;
         }
