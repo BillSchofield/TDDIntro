ec69f90162113108a6928fa98e15cb94c65210e8
Rebecca Eakins <rebecca.a.eakins@gmail.com>
2015-12-27 17:43:48 -0600

    all factorial tests pass

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -1,7 +1,9 @@
 package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
-    static Integer compute(int i) {
+    static Integer compute(int i) throws IllegalArgumentException {
+
+        if (i < 0) throw new IllegalArgumentException();
 
         if (i == 0 || i == 1) {
             return 1;
